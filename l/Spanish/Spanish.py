43cfbc66db8cbae99f536d974822626388e3f21d
Daniil <daniilsologu6@gmail.com>
2023-07-14 01:15:56 +0300

    Initial commit

---
+++ l/Spanish/Spanish.py
@@ -0,0 +1,85 @@
+from tkinter import *
+from tkinter import messagebox
+from gtts import gTTS
+import os
+import pandas as pd
+import random
+import playsound
+ 
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+unknown_words = {}
+ 
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/spanish_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+def is_known():
+    if len(to_learn) > 1:
+        to_learn.remove(current_card)
+        data = pd.DataFrame(to_learn)
+        data.to_csv("data/words_to_learn.csv", index=False)
+        next_card()
+    else:
+        messagebox.showinfo(title="Congratulation!",message="There's no word to learn!\nYou've review all the words!\nGood job, keep up the good work!")
+        os.remove("data/words_to_learn.csv")
+        window.quit()
+ 
+def next_card():
+    language = 'es'
+    global  current_card, flip_timer
+    current_card = random.choice(to_learn)
+    selected_card = current_card["Spanish"]
+    window.after_cancel(flip_timer)
+    canvas.itemconfig(card_title, text="Spanish", fill="black")
+    canvas.itemconfig(card_word, text=selected_card, fill="black")
+    canvas.itemconfig(card_side, image=card_front_img)
+    window.after(100)
+    audio_output = gTTS(text=current_card["Spanish"], lang=language)
+    audio_output.save("spanish_word.mp3")
+    playsound.playsound("spanish_word.mp3", False)
+    os.remove("spanish_word.mp3")
+    flip_timer = window.after(3000, func=flip_card)
+ 
+ 
+def flip_card():
+    language = 'en'
+    selected_card = current_card["English"]
+    canvas.itemconfig(card_side, image=card_back_img)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=selected_card, fill="white")
+    audio_output = gTTS(text=current_card["English"], lang=language)
+    audio_output.save("english_word.mp3")
+    playsound.playsound('english_word.mp3',False)
+    os.remove("english_word.mp3")
+ 
+window = Tk()
+window.title("BeeLingual(Vocabulary Building App for Spanish)")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+ 
+flip_timer = window.after(3000, func=flip_card)
+ 
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_side = canvas.create_image(400, 263,  image=card_front_img)
+card_title = canvas.create_text(400, 150, text="Spanish", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="word", font=("Ariel", 40, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+ 
+cross_button = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_button, highlightthickness=0, command=next_card, bd=0)
+unknown_button.grid(row=1, column=0)
+ 
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0, command=is_known, bd=0)
+known_button.grid(row=1, column=1)
+ 
+next_card()
+ 
+window.mainloop()
---
+++ main.py
@@ -0,0 +1,124 @@
+import os
+import sqlite3
+from tkinter import Tk, Label, Entry, Button, Toplevel, Canvas
+
+conn = sqlite3.connect('user_database.db')
+cursor = conn.cursor()
+cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                (id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                username TEXT NOT NULL,
+                email TEXT NOT NULL,
+                password TEXT NOT NULL)''')
+
+def login_user():
+    username = login_username_entry.get()
+
+    password = login_password_entry.get()
+    try:
+        cursor.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+        result = cursor.fetchone()
+        if result:
+            open_main_window()
+            first_window.withdraw()
+        else:
+            result_label.config(text="Invalid login credentials", fg="red")
+    except sqlite3.Error as error:
+        result_label.config(text="Error during login: " + str(error), fg="red")
+
+def register_user():
+    username = register_username_entry.get().strip()
+    email = register_email_entry.get().strip()
+    password = register_password_entry.get().strip()
+    try:
+        cursor.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (username, email, password))
+        conn.commit()
+        result_label.config(text="User registered successfully!", fg="green")
+    except sqlite3.Error as error:
+        result_label.config(text="Error registering user: " + str(error), fg="red")
+
+def open_main_window():
+    global main_window
+    main_window = Toplevel(first_window)
+    main_window.title("Main Window")
+    main_window.geometry("1920x1080")
+    main_window.configure(bg="#000")
+    welcome_label = Label(main_window, text="Welcome!", bg="#000", fg="white", font=("Arial", 32))
+    welcome_label.pack()
+    log_out_button = Button(main_window, text="Log Out", command=close_main_window, bg="#585858", fg="white", font=("Arial", 32))
+    log_out_button.pack()
+    account_button = Button(main_window, text="Account", command=close_main_window, bg="#585858", fg="white", font=("Arial", 32))
+    account_button.pack()
+    other_button = Button(main_window, text="Words", command=open_other_window, bg="#585858", fg="white", font=("Arial", 32))
+    other_button.pack()
+
+def open_other_window():
+    os.system("python Words/main.py")
+
+def close_main_window():
+    main_window.destroy()
+    first_window.deiconify()
+
+def close_gl_window():
+    gl_window.destroy()
+    first_window.deiconify() #??????????
+#################################
+gl_window = Tk()
+gl_window.title("Fluentify")
+gl_window.geometry("1920x1080")
+gl_window.configure(bg="#000")
+gl_label = Label(gl_window, text="\n \n \n \n \n \n \n \n Добро пожаловать в Fluentify! \n ", bg="#000", fg="white", font=("Arial", 32))
+gl_label.pack()
+
+gl_button = Button(gl_window, text="дальше", command=close_gl_window, bg="#585858", fg="white", font=("Arial", 32))
+gl_button.pack()
+
+
+first_window = Tk()
+first_window.title("Fluentify")
+first_window.geometry("1920x1080")
+first_window.configure(bg="#000")
+
+register_label = Label(first_window, text="Register:", bg="#000", fg="white", font=("Arial", 32))
+register_label.pack()
+
+register_username_label = Label(first_window, text="Username:", bg="#000", fg="white", font=("Arial", 32))
+register_username_label.pack()
+register_username_entry = Entry(first_window, font=("Arial", 32))
+register_username_entry.pack()
+
+register_email_label = Label(first_window, text="Email:", bg="#000", fg="white", font=("Arial", 32))
+register_email_label.pack()
+register_email_entry = Entry(first_window, font=("Arial", 32))
+register_email_entry.pack()
+
+register_password_label = Label(first_window, text="Password:", bg="#000", fg="white", font=("Arial", 32))
+register_password_label.pack()
+register_password_entry = Entry(first_window, show="*", font=("Arial", 32))
+register_password_entry.pack()
+
+register_button = Button(first_window, text="Register", command=register_user, bg="#585858", fg="white", font=("Arial", 32))
+register_button.pack()
+
+login_label = Label(first_window, text="Login:", bg="#000", fg="white", font=("Arial", 32))
+login_label.pack()
+
+login_username_label = Label(first_window, text="Username:", bg="#000", fg="white", font=("Arial", 32))
+login_username_label.pack()
+login_username_entry = Entry(first_window, font=("Arial", 32))
+login_username_entry.pack()
+
+
+
+login_password_label = Label(first_window, text="Password:", bg="#000", fg="white", font=("Arial", 32))
+login_password_label.pack()
+login_password_entry = Entry(first_window, show="*", font=("Arial", 32))
+login_password_entry.pack()
+
+login_button = Button(first_window, text="Login", command=login_user, bg="#585858", fg="white", font=("Arial", 32))
+login_button.pack()
+
+result_label = Label(first_window, text="", bg="#000", fg="white", font=("Arial", 32))
+result_label.pack()
+
+first_window.mainloop()
+conn.close()
