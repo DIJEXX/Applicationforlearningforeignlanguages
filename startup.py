60759a7937ae6b8c3da45190b272268c94c50e04
Daniil <daniilsologu6@gmail.com>
2023-07-17 14:13:27 +0300

    Update

--- startup.py
+++ startup.py
@@ -2,18 +2,18 @@ import os
 from tkinter import Tk, Label, Button
 
 
-def close_gl_window():
-    gl_window.destroy()
+def close_window():
+    window.destroy()
     os.system("python Words/main.py")
 
-gl_window = Tk()
-gl_window.state('zoomed')
-gl_window.title("Accelingvo")
-gl_window.iconbitmap('Words/py.ico')
-gl_window.geometry("1920x1080")
-gl_window.configure(bg="#000")
-gl_label = Label(gl_window, text="\n \n \n \n \n \n \n \n Добро пожаловать в Accelingvo! \n ", bg="#000", fg="white", font=("Arial", 32))
-gl_label.pack()
-gl_button = Button(gl_window, text="Дальше", command=close_gl_window, bg="#585858", fg="white", font=("Arial", 32))
-gl_button.pack()
-gl_window.mainloop()
+window = Tk()
+window.state('zoomed')
+window.title("Accelingvo")
+window.iconbitmap('Words/py.ico')
+window.geometry("1920x1080")
+window.configure(bg="#000")
+label = Label(window, text="\n \n \n \n \n \n \n \n Добро пожаловать в Accelingvo! \n ", bg="#000", fg="white", font=("Arial", 32))
+label.pack()
+button = Button(window, text="Дальше", command=close_window, bg="#585858", fg="white", font=("Arial", 32))
+button.pack()
+window.mainloop()
